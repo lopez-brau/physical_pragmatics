679b2f035abdee36f6cade6024acaf2dd3346831
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2018-01-22 05:06:19 -0500

    Renamed util to utils and added observer joint inference of enforcer's agent_rewards and p.

--- social_pragmatics.py
+++ social_pragmatics.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-from util import *
+from utils import *
 
 def agent_no_ToM(agent_reward, agent_cost, rationality):
 	U = agent_reward - agent_cost
@@ -58,7 +58,7 @@ def agent_ToM(agent_reward, agent_cost, enforcer_action, rationality, cooperatio
 
 	return action_probabilities
 
-def enforcer(enforcer_reward, rationality, p=0.0, cooperation=None, cache=False, plot=False):
+def enforcer(enforcer_reward, rationality, p=0.0, cooperation=None, cache=False, plot=False, ar=None):
 	# Set up the utility space.
 	space = tuple([MAX_VALUE for action in np.arange(NUM_ACTIONS)])
 	U = np.zeros(space)
@@ -77,6 +77,25 @@ def enforcer(enforcer_reward, rationality, p=0.0, cooperation=None, cache=False,
 	# Compute the utilities. Cached option only supports p = 1.0.
 	if cache == True:
 		U = retrieve_agent_ToM(agent_rewards, enforcer_reward, enforcer_actions, cooperation, U)
+	# Temporary? This runs the inference with agent reward fixed.
+	elif type(ar) != type(None):
+		U_no_ToM = np.zeros(space)
+		U_ToM = np.zeros(space)
+		agent_reward = ar
+		for enforcer_action in enforcer_actions:
+			# Reason about a ToM agent.
+			if p != 0.0:
+				agent_action_probabilities = agent_ToM(agent_reward, agent_cost, enforcer_action, rationality, \
+													   cooperation, cache=True)
+				expected_enforcer_reward = np.dot(enforcer_reward, agent_action_probabilities)
+				U_ToM[tuple(enforcer_action)] = expected_enforcer_reward - (COST_RATIO*sum(enforcer_action))
+
+			# Reason about a non-ToM agent.
+			updated_agent_cost = agent_cost + enforcer_action
+			agent_action_probabilities = agent_no_ToM(agent_reward, updated_agent_cost, rationality)
+			expected_enforcer_reward = np.dot(enforcer_reward, agent_action_probabilities)
+			U_no_ToM[tuple(enforcer_action)] = expected_enforcer_reward - (COST_RATIO*sum(enforcer_action))
+		U = ((1.0-p)*U_no_ToM) + (p*U_ToM)
 	else:
 		U_no_ToM = np.zeros(space)
 		U_ToM = np.zeros(space)
@@ -165,33 +184,92 @@ def observer(unobserved, rationality, **kwargs):
 		likelihood = np.zeros(space)
 
 		for c in np.arange(cooperation_set.size):
-			enforcer_action_probabilities = enforcer(
-				enforcer_reward=kwargs["enforcer_reward"],
-				rationality=rationality,
-				cooperation=cooperation_set[c],
-				smart=True,
-				cache=True
-			)
+			enforcer_action_probabilities = enforcer(enforcer_reward=kwargs["enforcer_reward"], rationality=rationality, \
+													 p=kwargs["p"], cooperation=cooperation_set[c])
 			likelihood[c] = enforcer_action_probabilities[tuple(kwargs["enforcer_action"])]
 
-			if sum(likelihood) == 0:
-				posterior = likelihood.reshape(space)
-			else:
-				posterior = likelihood / sum(likelihood)
+		if sum(likelihood) == 0:
+			posterior = likelihood.reshape(space)
+		else:
+			posterior = likelihood / sum(likelihood)
 		
 	# Infer whether the enforcer was acting for an agent with or without ToM.
-	# elif unobserved == "p":
+	elif unobserved == "p":
+		# Set up the space of possible proportion parameters and the likelihood
+		# space.
+		p_set = np.linspace(0.0, 1.0, num=11)
+		space = np.shape(p_set)
+		likelihood = np.zeros(space)
 
-	# 	p_set = np.linspace(0.0, 1.0, num=9)
+		for p in np.arange(p_set.size):
+			enforcer_action_probabilities = enforcer(enforcer_reward=kwargs["enforcer_reward"], rationality=rationality, \
+													 p=p, cooperation=kwargs["cooperation"])
+			likelihood[p] = enforcer_action_probabilities[tuple(kwargs["enforcer_action"])]
 
-	# 	for p in np.arange(p_set):
-	# 		enforcer_action_probabilities = enforcer(
-	# 			enforcer_reward=kwargs["enforcer_reward"],
-	# 			rationality=rationality,
-	# 			cooperation=cooperation_set[c],
-	# 			smart=p,
-	# 			cache=True
-	# 		)
+		if sum(likelihood) == 0:
+			posterior = likelihood.reshape(space)
+		else:
+			posterior = likelihood / sum(likelihood)
+
+	# Jointly infer what the enforcer's beliefs of the agent rewards and
+	# whether the enforcer was acting for an agent with or without ToM.
+	elif unobserved == "agent_rewards_and_p":
+		# Set up the space of possible proportion parameters and the likelihood
+		# space.
+		p_set = np.linspace(0.0, 1.0, num=11)
+		space = (100, p_set.size)
+		likelihood = np.zeros(space)
+
+		# Generate possible enforcer rewards. Use random sampling if the 
+		# problem space is bigger than MAX_SAMPLES.
+		size = min(np.prod(space), MAX_SAMPLES)
+		if size > MAX_SAMPLES:
+			agent_rewards = np.random.choice(MAX_VALUE, (MAX_SAMPLES, NUM_ACTIONS))
+		else:
+			agent_rewards = list(itertools.product(np.arange(MAX_VALUE), repeat=NUM_ACTIONS))
+
+		# Compute the likelihood.
+		for i in np.arange(len(agent_rewards)):
+			for p in np.arange(p_set.size):
+				enforcer_action_probabilities = enforcer(enforcer_reward=kwargs["enforcer_reward"], rationality=rationality, \
+													 	 p=p, cooperation=kwargs["cooperation"], ar=agent_rewards[i])
+				likelihood[i][p] = enforcer_action_probabilities[tuple(kwargs["enforcer_action"])]
+
+		# Normalize the likelihood to generate the posterior.
+		likelihood = likelihood.flatten()
+		if sum(likelihood) == 0:
+			posterior = likelihood.reshape(space)
+		else:
+			posterior = (likelihood/sum(likelihood)).reshape(space)
+
+	elif unobserved == "enforcer_and_agent_rewards":
+		# Set up the likelihood space.
+		space = (100, 10, 10)
+		likelihood = np.zeros(space)
+
+		# Generate possible enforcer rewards. Use random sampling if the 
+		# problem space is bigger than MAX_SAMPLES.
+		size = min(np.prod(space), MAX_SAMPLES)
+		if size > MAX_SAMPLES:
+			agent_rewards = np.random.choice(MAX_VALUE, (MAX_SAMPLES, NUM_ACTIONS))
+			enforcer_rewards = np.random.choice(MAX_VALUE, (MAX_SAMPLES, NUM_ACTIONS))
+		else:
+			agent_rewards = list(itertools.product(np.arange(MAX_VALUE), repeat=NUM_ACTIONS))
+			enforcer_rewards = list(itertools.product(np.arange(MAX_VALUE), repeat=NUM_ACTIONS))
+		
+		# Compute the likelihood.
+		for i in np.arange(len(agent_rewards)):
+			for enforcer_reward in enforcer_rewards:
+				enforcer_action_probabilities = enforcer(enforcer_reward, rationality, cooperation=kwargs["cooperation"], \
+														 cache=True, ar=agent_rewards[i])
+				likelihood[i][tuple(enforcer_reward)] = enforcer_action_probabilities[tuple(kwargs["enforcer_action"])]
+
+		# Normalize the likelihood to generate the posterior.
+		likelihood = likelihood.flatten()
+		if sum(likelihood) == 0:
+			posterior = likelihood.reshape(space)
+		else:
+			posterior = (likelihood/sum(likelihood)).reshape(space)
 
 	# Plot the action probabilities.
 	if kwargs["plot"] == True:
@@ -204,22 +282,27 @@ def observer(unobserved, rationality, **kwargs):
 	return posterior
 
 def main():
-	# agent_reward = np.array((5, 0))
+	agent_reward = np.array((0, 9))
 	# agent_cost = np.array((0, 0))
 	enforcer_reward = np.array((0, 9))
-	# enforcer_action = np.array((2, 0))
+	enforcer_action = np.array((2, 0))
 
 	rationality = 1.0
 	cooperation = 2.0
-	
-	start_time = time.time()
+	p = 1.0
+
 	# x1 = enforcer(enforcer_reward, rationality, plot=True))
-	x2 = enforcer(enforcer_reward, rationality, cooperation=cooperation, cache=True, plot=True)
-	x3 = enforcer(enforcer_reward, rationality, p=1.0, cooperation=cooperation, cache=False, plot=True)
-	x4 = observer("enforcer_reward", rationality, cooperation)
-	print(x4)
+	# x2 = enforcer(enforcer_reward, rationality, cooperation=cooperation, cache=True, plot=True)
+	# x3 = enforcer(enforcer_reward, rationality, p=1.0, cooperation=cooperation, cache=False, plot=True, ar=agent_reward)
+	start_time = time.time()
+	x4 = observer("agent_rewards_and_p", rationality, enforcer_reward=enforcer_reward, enforcer_action=enforcer_action, p=p,
+				  cooperation=cooperation, plot=False)
 	print(time.time()-start_time)
-	plt.show()
+	print(x4)
+	with open("temp.txt", "w", newline="") as file:
+		writer = csv.writer(file)
+		writer.writerows(x4)
+	plt.show()	
 	return
 
 if __name__ == "__main__":
