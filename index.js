ae76374d9605fa2f36dd65b22291c95ab97dacf3
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2018-04-08 10:51:50 -0400

    Uploading low-plant condition.

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ function init() {
     // Select whether the door is open or closed.
     exp.door = _.sample(["closed", "open"]);
     // exp.cost = _.sample(["low", "none"]);
-    exp.cost = "none";
+    exp.cost = "low";
     // exp.object = _.sample(["chair", "plant"]);
     exp.object = "plant";
     $(".display_object").html(exp.object);
