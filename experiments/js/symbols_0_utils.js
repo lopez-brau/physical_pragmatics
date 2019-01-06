397747a55884b234178617f7d5d52a9af862ecdc
Michael Lopez-Brau <michael.lopez.brau@gmail.com>
2019-01-05 21:16:49 -0500

    Fixed string/fishbowl issue.

--- experiments/js/symbols_0_utils.js
+++ experiments/js/symbols_0_utils.js
@@ -77,7 +77,7 @@ function get_noun_phrase_1(condition, object) {
         "tape": ((condition == "symbol") ? "a picture of " : "") + "some tape",
         "rulers": ((condition == "symbol") ? "a picture of " : "") + "some rulers taped to the door frame",
         "hat": ((condition == "symbol") ? "a picture of " : "") + "a hat",
-        "string": ((condition == "symbol") ? "a picture of " : "") + "a fishbowl tied to a string"
+        "fishbowl": ((condition == "symbol") ? "a picture of " : "") + "a fishbowl tied to a string"
     }
 
     return noun_phrases[object]
